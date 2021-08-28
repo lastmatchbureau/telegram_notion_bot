9a9a5c80191c9b163212ba3b061856a887a4c355
Oleg Smarandi <olegsmarandi@gmail.com>
2021-08-28 09:51:49 +0300

    bot v0.9.9.0 File uploading: changed upload from api.helper server to local server

--- file_manager.py
+++ file_manager.py
@@ -8,17 +8,18 @@ class FileManager:
     def __init__(self, token, downloaded_files_folder_name="download"):
         self.api_token = token
         self.downloaded_files_folder_name = downloaded_files_folder_name
+        self.base_url = f"http://0.0.0.0:8081/bot{self.api_token}"
 
     def upload_file(self, chat_id, file_path):
         file_size = (Path(file_path).stat().st_size / 1024) / 1024
         if file_size <= 50:
             print(f"sending {file_path} with size: {file_size} mb")
-            url = f'https://api.telegram.org/bot{self.api_token}/sendDocument?chat_id={chat_id}'
+            url = f'{self.base_url}/sendDocument?chat_id={chat_id}'
             r = requests.post(url, files={"document": open(file_path, 'rb')})  # note: files, not data
             if r.status_code == 200:
                 print(f"sent {file_path} with size: {file_size} mb")
         else:
-            url = f'https://api.telegram.org/bot{self.api_token}/sendMessage'
+            url = f'{self.base_url}/sendMessage'
             r = requests.post(url, json={"text": f"{file_path.replace('download/', '')} is bigger than 50 mb.",
                                          "chat_id": chat_id},)
         remove(file_path)
