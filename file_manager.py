81e2793472132f45624d65e7f40d552b56f09db7
Oleg Smarandi <olegsmarandi@gmail.com>
2021-08-28 10:38:35 +0300

    bot v0.9.9.1 File Manager: Cosmetic Change in bot behavior during uploading large files

--- file_manager.py
+++ file_manager.py
@@ -10,17 +10,33 @@ class FileManager:
         self.downloaded_files_folder_name = downloaded_files_folder_name
         self.base_url = f"http://0.0.0.0:8081/bot{self.api_token}"
 
-    def upload_file(self, chat_id, file_path):
-        file_size = (Path(file_path).stat().st_size / 1024) / 1024
-        print(f"sending {file_path} with size: {file_size} mb")
+    @staticmethod
+    def __get_file_name(file_path, chat_id):
+        return file_path.replace(f'download/{chat_id}', '')
+
+    @staticmethod
+    def __get_file_size(file_path):
+        return (Path(file_path).stat().st_size / 1024) / 1024
+
+    def __send_message(self, chat_id, text):
+        url = f'{self.base_url}/sendMessage'
+        r = requests.post(url, json={"text": text,
+                                     "chat_id": chat_id}, )
+
+    def __send_document(self, chat_id, file_path):
+        file_name = self.__get_file_name(file_path, chat_id)
         url = f'{self.base_url}/sendDocument?chat_id={chat_id}'
         r = requests.post(url, files={"document": open(file_path, 'rb')})  # note: files, not data
         if r.status_code == 200:
-            print(f"sent {file_path} with size: {file_size} mb")
+            print(f"sent {file_path}")
         else:
-            url = f'{self.base_url}/sendMessage'
-            r = requests.post(url, json={"text": f"{file_path.replace(f'download/{chat_id}', '')} can not be sent",
-                                         "chat_id": chat_id},)
+            self.__send_message(chat_id, text=f"{file_name} can not be sent")
+
+    def upload_file(self, chat_id, file_path):
+        file_size = self.__get_file_size(file_path)
+        file_name = self.__get_file_name(file_path, chat_id)
+        print(f"sending {file_path} with size: {file_size} mb")
+        self.__send_message(chat_id, text=f"Отправляю {file_name} с размером: {file_size} mb.")
         remove(file_path)
 
     def delete_downloaded_files(self, tg_id):
